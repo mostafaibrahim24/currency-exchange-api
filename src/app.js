d66d74e98f3750e183fb1341d63f1cae92d9b1d3
mostafaibrahim24 <mi.mostafaibrahim@gmail.com>
2024-06-21 22:51:40 +0300

    Implement rate limiting for the API to prevent abuse

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const rl = require('express-rate-limit')
+
 
 
 const routes =require("./route")
@@ -8,11 +10,17 @@ const {validateRequest,changeCurrencyCase} = require('./middleware')
 
 const app = express();
 
+const limiter = rl({
+	windowMs: 15 * 60 * 1000, // 15 mins
+	max: 100, // IP has 100 requests over 15 mins
+})
+
 const dotenv = require('dotenv');
 dotenv.config();
 
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(limiter)
 
 
 
