2bb2c5c981ca76c38c117998112c6aa0b20aadef
mostafaibrahim24 <mi.mostafaibrahim@gmail.com>
2024-06-21 12:35:19 +0300

    Validate request (not allowed and required) and Implementing Error handling

--- src/controller.js
+++ src/controller.js
@@ -32,11 +32,9 @@ function formatResponse(result, is_conversion=false,amount=1) {
         }
     }else{
         var item = {}
-        console.log(Object.keys(result))
         
         for (let i = 0; i < Object.keys(result).length; i++){
                 var key = Object.keys(result)[i]
-                console.log(i,key)
                 item['from']=key.split("_")[0]
                 item['to']=key.split("_")[1]
                 item['rate']=result[key]
@@ -46,34 +44,81 @@ function formatResponse(result, is_conversion=false,amount=1) {
     }
     return data
 }
+function validateRequest(req,is_conversion=false){
+    var allowed_properties = ['from_currency','to_currency']
+    if(is_conversion){
+        allowed_properties.push('amount')
+    }
+    const body=req.body
+    const sent_properties = Object.keys(body)
+    const not_allowed_properties_sent = sent_properties.filter(x => !allowed_properties.includes(x))
+    const required_properties_not_sent = allowed_properties.filter(x=> !sent_properties.includes(x))
+    if (not_allowed_properties_sent.length>0 && required_properties_not_sent.length>0 ){
+        console.error("Request: "+req.originalUrl+" ,Required properties/parameters not sent in body: "+required_properties_not_sent+" ,Not allowed properties/parameters sent in body: "+not_allowed_properties_sent)
+        throw {
+            "Error": "Please adhere to API's required and allowed properties in body",
+            "required": required_properties_not_sent,
+            "not_allowed": not_allowed_properties_sent
+        }
+    }
+    if (not_allowed_properties_sent.length>0){
+        console.error("Request: "+req.originalUrl+" ,Not allowed properties/parameters sent in body: "+not_allowed_properties_sent)
+        throw {
+            "Error": "Please adhere to API's allowed properties in body",
+            "not_allowed": not_allowed_properties_sent
+        }
+    }
+    if (required_properties_not_sent.length>0){
+        console.error("Request: "+req.originalUrl+" ,Required properties/parameters not sent in body: "+required_properties_not_sent)
+        throw {
+            "Error": "Please adhere to API's required properties in body",
+            "required": required_properties_not_sent
+        }
+    }
+    
+}
 
 const getExchangeRate = async(req,res) => {
     try{
+        validateRequest(req)
+    }catch(error){
+        return res.status(400).json(error)
+    }
+    var result = {}
+    try{
         const external_api_response = await callExternalAPI(req)
         var result = await external_api_response.json();
-        var data=formatResponse(result)
-        return res.status(200).json(data)
-    } catch (error){
-
+    }catch(error){
+        console.error("Error calling external api: ",error)
+        return res.status(500).json({Error: "Unexpected error occurred, try again after a while."})
     }
+    var data=formatResponse(result)
+    return res.status(200).json(data)
 }
 
 const convert = async (req,res) => {
+    const is_conversion = true
     try{
-        const amount = req.body.amount
+        validateRequest(req,is_conversion)
+    }catch(error){
+        return res.status(400).json(error)
+    }
+    const amount = req.body.amount
+    var result = {}
+    try{
         const external_api_response = await callExternalAPI(req)
         var result = await external_api_response.json();
-        
-        for (let i = 0; i < Object.keys(result).length; i++){
-            var key = Object.keys(result)[i]
-            result[key]=result[key] * Number(amount)
-        }
-        var data=formatResponse(result,true,amount)
-        return res.status(200).json(data)
-        
-    } catch (error){
-
+    }catch(error){
+        console.error("Error calling external api: ",error)
+        return res.status(500).json({Error: "Unexpected error occurred, try again after a while."})
     }
+    for (let i = 0; i < Object.keys(result).length; i++){
+        var key = Object.keys(result)[i]
+        result[key]=result[key] * Number(amount)
+    }
+    var data=formatResponse(result,is_conversion,amount)
+    return res.status(200).json(data)
+
 }
   
 
